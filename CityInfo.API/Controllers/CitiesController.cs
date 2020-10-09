1232d67a3d38163d3b39b82801ee18ac4afc6962
Roland Zabos <roland.zabos@ni.com>
2020-10-09 15:24:02 +0200

    Add project files.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
---
+++ CityInfo.API/Models/CityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CityInfo.API.Models
+{
+    public class CityDto
+    {
+        public string Description { get; set; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
